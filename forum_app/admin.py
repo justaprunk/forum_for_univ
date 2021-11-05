97a4b11781b3fe4ff9f51528b58679d9e86bad82
Otsu <71006363+Error1368@users.noreply.github.com>
2021-11-05 11:48:21 +0300

    refactoring models and setup admin panel

--- forum_app/admin.py
+++ forum_app/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from .models import User, Article, Activity, Comment
 
-admin.site.register(User)
-admin.site.register(Article)
-admin.site.register(Activity)
-admin.site.register(Comment)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', "profile_photo"]
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "rating", "title_image"]
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ["author", "activity_type", "comment_parent",
+                    'article_parent']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["author", "rating", "comment_parent", 'article_parent']
+
--- forum_app/models.py
+++ forum_app/models.py
@@ -6,8 +6,8 @@ from django.core.exceptions import ValidationError
 class User(models.Model):
     username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=64)
-    profile_photo = models.ImageField(upload_to = 'profiles/', default=None, null=True,
-        blank=True)
+    profile_photo = models.ImageField(upload_to='profiles/', default=None,
+                                      null=True, blank=True)
 
     def __str__(self):
         return self.username
@@ -20,39 +20,58 @@ class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     deploy_time = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=50)
-    title_image = models.ImageField(default=None, upload_to = 'articles/', null=True,
-        blank=True)
+    title_image = models.ImageField(default=None, upload_to='articles/',
+                                    null=True, blank=True)
     text = models.CharField(max_length=500)
 
     class Meta:
         ordering = ['author', '-deploy_time']
 
     def __str__(self):
-        return f"{self.title} Author: {self.author.username if self.author else None}, " \
-               f"ID: {self.id}"
+        return f"{self.title}"
+
+    def rating(self):
+        rating = 0
+        for activity in Activity.objects.filter(article_parent=self):
+            if activity.activity_type == 'L':
+                rating += 1
+            elif activity.activity_type == 'D':
+                rating -= 1
+        return rating
 
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    article_parent = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, blank=True)
-    comment_parent = models.ForeignKey('Comment', on_delete=models.CASCADE, null=True,
-        blank=True)
+    article_parent = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                       null=True, blank=True)
+    comment_parent = models.ForeignKey('Comment', on_delete=models.CASCADE,
+                                       null=True, blank=True)
     text = models.CharField(max_length=500)
     deploy_time = models.DateTimeField(auto_now_add=True)
-    attachment = models.FileField(default=None, upload_to = 'attachments/', null=True,
-        blank=True)
+    attachment = models.FileField(default=None, upload_to='attachments/',
+                                  null=True, blank=True)
 
     def __str__(self):
-        return f"{self.author.username if self.author else None} comments " \
-               f"{self.article_parent.__str__() if self.article_parent else self.comment_parent.__str__() if self.comment_parent else None}"
+        return f"comment for " \
+               f"{self.article_parent if self.article_parent else 'another comment'}"
 
+    def rating(self):
+        rating = 0
+        for activity in Activity.objects.filter(comment_parent=self):
+            if activity.activity_type == 'L':
+                rating += 1
+            elif activity.activity_type == 'D':
+                rating -= 1
+        return rating
 
     def clean(self):
-        if (self.comment_parent and self.article_parent) or (not self.comment_parent and not self.article_parent):
+        if (self.comment_parent and self.article_parent) or \
+                (not self.comment_parent and not self.article_parent):
             raise ValidationError("Comment xor article is required.")
+        super().clean()
 
     class Meta:
-        ordering = ['article_parent', 'comment_parent', '-deploy_time']
+        ordering = ['-deploy_time']
 
 
 class Activity(models.Model):
@@ -62,18 +81,22 @@ class Activity(models.Model):
         ('D', 'Dislike'),
     )
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    article_parent = models.ForeignKey(Article, on_delete=models.CASCADE, null=True)
-    comment_parent = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True)
+    article_parent = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                       null=True, blank=True)
+    comment_parent = models.ForeignKey(Comment, on_delete=models.CASCADE,
+                                       null=True, blank=True)
     activity_type = models.CharField(max_length=1, choices=ACTIVITY_TYPES)
 
     def __str__(self):
-        return f"{self.author.username if self.author else None} {self.activity_type} " \
-               f"{self.article_parent.__str__() if self.article_parent else self.comment_parent.__str__() if self.comment_parent else None}"
+        return f"{self.activity_type}"
 
     def clean(self):
-        if (self.comment_parent and self.article_parent) or (not self.comment_parent and not self.article_parent):
+        if (self.comment_parent and self.article_parent) or \
+                (not self.comment_parent and not self.article_parent):
             raise ValidationError("Comment xor article is required.")
+        super().clean()
 
     class Meta:
         ordering = ['author', '-activity_type']
-        unique_together = (('author', 'article_parent', 'comment_parent', 'activity_type'),)
+        unique_together = (('author', 'article_parent',
+                            'comment_parent', 'activity_type'),)
